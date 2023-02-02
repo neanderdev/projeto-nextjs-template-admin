79baa17aedde467acc3c6741b69a11201c08ef68
Neander de Souza <neander.faria@hotmail.com>
2023-02-02 15:39:11 -0300

    Botão Alternar Tema #02

--- src/components/template/BotaoAlternarTema.tsx
+++ src/components/template/BotaoAlternarTema.tsx
@@ -1,4 +1,4 @@
-import { IconeSol } from '../icons';
+import { IconeLua, IconeSol } from '../icons';
 
 interface BotaoAlternarTemaProps {
     tema: string;
@@ -9,18 +9,30 @@ export default function BotaoAlternarTema({ tema, alternarTema }: BotaoAlternarT
     return tema === 'dark'
         ? (
             <div
+                className='hidden sm:flex items-center bg-gradient-to-t from-yellow-300 to-yellow-600 w-14 lg:w-24 h-8 p-1 rounded-full cursor-pointer'
                 onClick={alternarTema}
             >
-                <div>
-                    {IconeSol}
+                <div className='flex items-center justify-center bg-white text-yellow-600 w-6 h-6 rounded-full'>
+                    {IconeSol(4)}
                 </div>
 
-                <div>
-                    <span>Claro</span>
+                <div className='hidden lg:flex items-center ml-4 text-white'>
+                    <span className='text-sm'>Claro</span>
                 </div>
             </div>
         )
         : (
-            <div></div>
+            <div
+                className='hidden sm:flex items-center justify-end bg-gradient-to-t from-gray-500 to-gray-900 w-14 lg:w-24 h-8 p-1 rounded-full cursor-pointer'
+                onClick={alternarTema}
+            >
+                <div className='hidden lg:flex items-center mr-2 text-gray-300'>
+                    <span className='text-sm'>Escuro</span>
+                </div>
+
+                <div className='flex items-center justify-center bg-black text-yellow-600 w-6 h-6 rounded-full'>
+                    {IconeLua(4)}
+                </div>
+            </div>
         )
 }
